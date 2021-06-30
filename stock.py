8a805dbb1cacae21b3dc8d9530acaed0f13474aa
Mike Tanaka <wilco42@gmail.com>
2021-06-30 16:54:22 -0700

    add symbol to stock_info

--- stock.py
+++ stock.py
@@ -1,16 +1,27 @@
 from datetime import date
 import lib.image as image
-import lib.stock as stock
+#import lib.stock as stock
+
+#stock_info = stock.get_stock_info('WDAY')
+#print (stock_info)
+
+stock_info = {'symbol': 'WDAY', 'current_price': 238.74, 'day_diff': -7.65, 'percent': -3.10}
 
 card = image.initImage()
-image.drawText(card, font = "fonts/5x8.pil", color = (200, 200, 200), text = 'WDAY' , offset = (1, 1));
+# ticker
+image.drawText(card, font = "fonts/5x8.pil", color = (200, 200, 200), text = stock_info['symbol'] , offset = (1, 1));
 
-image.drawText(card, font = "fonts/5x8.pil", color = (200, 200, 200), text = '238', offset = (1, 8));
-image.drawText(card, font = "fonts/5x8.pil", color = (200, 200, 200), text = '74', offset = (18, 8));
+# current price
+current = stock_info['current_price'].split(".")
+image.drawText(card, font = "fonts/5x8.pil", color = (200, 200, 200), text = current[0], offset = (1, 8));
+image.point([(13, 1)], fill = (200, 200, 200))
+image.drawText(card, font = "fonts/5x8.pil", color = (200, 200, 200), text = current[1], offset = (18, 8));
 
+# day's diff
 image.drawText(card, font = "fonts/5x8.pil", color = (200, 0, 0), text = '7', offset = (43, 1));
 image.drawText(card, font = "fonts/5x8.pil", color = (200, 0, 0), text = '65', offset = (50, 1));
 
+# percent diff
 image.drawText(card, font = "fonts/5x8.pil", color = (200, 0, 0), text = '3', offset = (43, 8));
 image.drawText(card, font = "fonts/5x8.pil", color = (200, 0, 0), text = '10%', offset = (49, 8));
 card.save('cards/stock.png')
