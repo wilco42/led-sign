7f2fd1ca0c7c83e28eead651ed15c86fcf47267c
Mike Tanaka <wilco42@gmail.com>
2021-06-29 19:02:10 -0700

    last changes into the viewer

--- image-viewer.py
+++ image-viewer.py
@@ -18,8 +18,10 @@ options.rows = 32
 options.cols = 64
 options.chain_length = 1
 options.parallel = 1
-#options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
+# with pwm slowdown to 4 seems to work well for RPi0
+options.gpio_slowdown = 4
 options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
+#options.hardware_mapping = 'adafruit-hat-pwm'  # If you have an Adafruit HAT: 'adafruit-hat'
 
 matrix = RGBMatrix(options = options)
 
